a3dc1f7e3db63bd43bd43ae3790226b47bf6a203
Yuang <yuangshi@u.nus.edu>
2024-03-20 20:03:53 +0800

    .

--- script.py
+++ script.py
@@ -93,16 +93,16 @@ torch.cuda.empty_cache()
 render_script = "/home/yuang/Desktop/gaussian-splatting/render.py"
 eva_script = "/home/yuang/Desktop/gaussian-splatting/metrics.py"
 
-print(f"------------\nRendering {output_dir}\n------------")
+print(f"------------------------\nRendering {output_dir}\n------------------------")
 command = [
     'python', render_script,
     '-m', output_dir,
-    # '--skip_train',
+    '--skip_train',
     ]
 subprocess.run(command)
 torch.cuda.empty_cache()
 
-print(f"------------\nEvaluating {output_dir}\n------------")
+print(f"------------------------\nEvaluating {output_dir}\n------------------------")
 command = [
     'python', eva_script,
     '-m', output_dir,
