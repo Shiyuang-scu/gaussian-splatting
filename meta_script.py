8211e9a1aa5ae51c33fba330db57f6b320b5f230
Yuang <yuangshi@u.nus.edu>
2024-03-25 17:07:22 +0800

    Add denoise option to script.py

--- meta_script.py
+++ meta_script.py
@@ -1,12 +1,13 @@
 import subprocess
 
-size_of_subset_list = [500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250]
+size_of_subset_list = [200, 500, 750, 1000, 1250, 1500, 1750, 2000, 2250, 2500, 2750, 3000, 3250]
 
 for size_of_subset in size_of_subset_list:
     print("Running on subset of size", size_of_subset)
     command = [
         'python', "script.py",
         '--size_of_subset', str(size_of_subset)
+        '--denoise',
         ]
 
     subprocess.run(command)
